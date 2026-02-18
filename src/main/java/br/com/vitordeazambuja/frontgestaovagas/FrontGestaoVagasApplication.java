c6d7d2e88090c5a11f23514a26a1c067891f53f4
Vitor <vitordeazambuja@gmail.com>
2026-02-18 20:07:44 -0300

    initial commit

---
+++ src/main/java/br/com/vitordeazambuja/frontgestaovagas/FrontGestaoVagasApplication.java
@@ -0,0 +1,13 @@
+package br.com.vitordeazambuja.frontgestaovagas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontGestaoVagasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontGestaoVagasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/vitordeazambuja/frontgestaovagas/FrontGestaoVagasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vitordeazambuja.frontgestaovagas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontGestaoVagasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
