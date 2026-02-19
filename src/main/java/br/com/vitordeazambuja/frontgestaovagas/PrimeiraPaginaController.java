00cdfcfae21982a3736b668db9685de5055d39c1
Vitor <vitordeazambuja@gmail.com>
2026-02-19 15:44:05 -0300

    chore: envia dados do formulario para o controller

--- src/main/java/br/com/vitordeazambuja/frontgestaovagas/PrimeiraPaginaController.java
+++ src/main/java/br/com/vitordeazambuja/frontgestaovagas/PrimeiraPaginaController.java
@@ -3,6 +3,7 @@ package br.com.vitordeazambuja.frontgestaovagas;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,5 +21,10 @@ public class PrimeiraPaginaController {
     @GetMapping("/login")
     public String loginCandidate(){
         return "candidate/login";
+    }
+
+    @PostMapping("/create")
+    public String cadastroCandidate(String nome_do_candidate){
+        return "/candidate/login";
     }
 }
