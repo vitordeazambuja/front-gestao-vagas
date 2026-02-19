69e4d39886c6334baa1b20e5634a08b27ec13248
Vitor <vitordeazambuja@gmail.com>
2026-02-19 16:50:31 -0300

    chore: cria pagina de login da aplicacao principal

---
+++ src/main/java/br/com/vitordeazambuja/frontgestaovagas/modules/candidate/controller/CandidateController.java
@@ -0,0 +1,15 @@
+package br.com.vitordeazambuja.frontgestaovagas.modules.candidate.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/candidate")
+public class CandidateController {
+
+    @GetMapping("/login")
+    public String login(){
+        return "candidate/login";
+    }
+}
